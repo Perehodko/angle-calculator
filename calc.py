cc77510f43a1d975f5c57dea55879c8efc675d7f
Perehodko <n.perehodko@yandex.com>
2020-06-12 22:49:45 +0300

    add clever print

--- calc.py
+++ calc.py
@@ -7,7 +7,8 @@ fh = open(fname)
 
 for line in fh:
     line_r = line.rstrip()
-    # pos ang
+
+    #pos ang
     pos_angl = line_r.find("ᵒ")
     angler = line_r[:pos_angl]
     # pos minute
@@ -25,9 +26,23 @@ for line in fh:
     print("="*40, sep='\n')
     # print("angler: " + angler, "minute: " + minute, "sec: "+ sec)
     print("original str:", line_r)
+
     print("-"*40)
-    print("            degree -->" , '%.3f' %degree)
-    print("   mask 000ᵒ00'00\" -->", "{0}ᵒ{1}'{2}\"".format(angler,minute,sec))
-    print("mask 000ᵒ00'00.00\" -->", "{0}ᵒ{1}'{2}.{3}\"".format(angler,minute,sec, "00"))
-    print("           radians -->", '%.1f' % radians)
-    print("                GD -->", "{0}-{1}".format(int(GD), str(GD)[2:4]))
+    print("            degree -->" , '%.5f' %degree)
+    if "." in angler:
+        print("   mask 000ᵒ00'00\" -->", "{0}ᵒ{1}'{2}\"".format(int(float(angler)), minute, sec))
+        print("mask 000ᵒ00'00.00\" -->", "{0}ᵒ{1}'{2}.{3}\"".format(int(float(angler)), minute, sec, "00"))
+    elif "." in minute:
+        print("   mask 000ᵒ00'00\" -->", "{0}ᵒ{1}'{2}\"".format(angler, int(float(minute)), sec))
+        print("mask 000ᵒ00'00.00\" -->", "{0}ᵒ{1}'{2}.{3}\"".format(angler, int(float(minute)), sec, "00"))
+    elif "." in sec:
+        print("   mask 000ᵒ00'00\" -->", "{0}ᵒ{1}'{2}\"".format(angler, minute, int(float(sec))))
+        print("mask 000ᵒ00'00.00\" -->", "{0}ᵒ{1}'{2}\"".format(angler, minute, float(sec)))
+    else:
+        print("   mask 000ᵒ00'00\" -->", "{0}ᵒ{1}'{2}\"".format(angler,minute,sec))
+        print("mask 000ᵒ00'00.00\" -->", "{0}ᵒ{1}'{2}.{3}\"".format(angler,minute,sec, "00"))
+
+
+    print("           radians -->", '%.2f' % radians)
+    GD_i = str(GD).find(".")
+    print("                GD -->", "{0}-{1}".format(int(GD), str(GD)[GD_i+1:GD_i+3:]))
