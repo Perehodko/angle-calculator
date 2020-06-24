82de270157240bafc71fa255bbe51b399b09c154
Perehodko <n.perehodko@yandex.com>
2020-06-24 23:37:51 +0300

    add GD and all calculate

--- diff_anglers.py
+++ diff_anglers.py
@@ -1,3 +1,4 @@
+import math
 fname = input("Enter file name: ")
 if len(fname) < 1: fname = "test_diff.txt"
 
@@ -6,8 +7,10 @@ store_list = []
 count = 0
 for line in fh:
     line_r = line.rstrip()
-    print("   ", line_r)
-
+    if not line_r.isspace():
+        print("   ", line_r)
+        if count == 0:
+            print("-"*35)
 
     if "ᵒ" in line_r:
         # pos ang
@@ -23,7 +26,16 @@ for line in fh:
         d1 = float(angler) + (float(minute) * 0.0166666667) + (float(sec) * 0.000277778)
         store_list.append(d1)
         count += 1
-    if count == 2:
+    if "-" in line_r:
+        # float from GD
+        num = float(line_r.replace('-', '.'))
+        angler_from_GD = num * 6
+
+        d1 = angler_from_GD
+        store_list.append(d1)
+        count += 1
+
+    if count == 2 and "ᵒ" in line_r:
         diff_ang = store_list[0]-store_list[1]
         store_list = []
         count = 0
@@ -33,7 +45,7 @@ for line in fh:
         #min
         min_tenth = diff_ang - ang
         min = min_tenth * 60
-        if min < 60:
+        if min > 60:
              ang -= 1
              min = 60 - abs(min)
 
@@ -45,9 +57,70 @@ for line in fh:
         #      sec = 60 - abs(sec)
 
         if diff_ang < 0:
-            # print(diff_ang)
+        # print(diff_ang)
             diff_ang = 360 - abs(diff_ang)
-            print("-->", "{0}ᵒ{1}'{2}\"".format(int(diff_ang), int(min), round(sec)))
-        else:
-            # sum
-            print("-->", "{0}ᵒ{1}'{2}\"".format(int(diff_ang), int(min), round(sec)))
+        if min < 0:
+            min = 60 - abs(min)
+        if sec < 0:
+            sec = 60 - abs(sec)
+
+        # radians
+        radians = (float(diff_ang) * math.pi) / 180
+
+        # БДУ
+        GD = float(diff_ang) / 6
+
+        print("-" * 35, sep='\n')
+        print("diff", "{0}ᵒ{1}'{2}\"".format(int(diff_ang), int(min), round(sec)))
+        print("-" * 35, sep='\n')
+        print("            degree -->", '%.5f' % diff_ang)
+        print("   mask 000ᵒ00'00\" -->", "{0}ᵒ{1}'{2}\"".format(int(diff_ang), int(min), int(float(sec))))
+        print("   mask 000ᵒ00.00' -->", "{0}ᵒ{1}'".format(int(diff_ang), round(min, 2)))
+        print("           radians -->", '%.2f' % radians)
+        print("                GD -->", (str(round(GD, 2)).replace(".", '-')))
+        print("-" * 35, sep='\n')
+        print("\n")
+    if count == 2 and "-" in line_r:
+        diff_ang = store_list[0]-store_list[1]
+        # print(diff_ang)
+        if diff_ang < 0:
+            diff_ang = 360 - abs(diff_ang)
+            # print(diff_ang)
+
+        diff_ang = diff_ang / 6
+        store_list = []
+        count = 0
+        # float минуты
+        min_f_from_GD = diff_ang - int(diff_ang)
+
+        min_from_GD = min_f_from_GD * 60
+        # целые минуты
+        int_min_GD = int(min_from_GD)
+        # доли минут для перевода в секунды
+        min_GD_float = min_from_GD - int_min_GD
+        # перевод долей градуса в секунды
+        sec_GD = min_GD_float * 60
+
+        if math.ceil(sec_GD * 100) / 100 >= 60:
+            int_min_GD += 1
+            sec_GD = 0
+        # минуты с долями
+        min_sec = int_min_GD + sec_GD / 60
+        # print(angler_from_GD)
+
+        # radians
+        radians_GD = (float(diff_ang) * math.pi) / 180
+
+
+        print("-" * 35, sep='\n')
+        # print("original str:", line_r)
+        print("diff", (str(round(diff_ang, 2)).replace(".", '-')))
+        print("-" * 35)
+        print("            degree -->", '%.5f' % diff_ang)
+        print("   mask 000ᵒ00'00\" -->", "{0}ᵒ{1}'{2}\"".format(int(diff_ang), int_min_GD, int(float(sec_GD))))
+        print("   mask 000ᵒ00.00' -->", "{0}ᵒ{1}'".format(int(diff_ang), round(min_sec, 2)))
+        print("           radians -->", '%.2f' % radians_GD)
+        print("                GD -->", (str(round(diff_ang, 2)).replace(".", '-')))
+        print("-" * 35, sep='\n')
+        print("\n")
+
--- sum_angles.py
+++ sum_angles.py
@@ -1,3 +1,5 @@
+import math
+
 fname = input("Enter file name: ")
 if len(fname) < 1: fname = "test_sum.txt"
 
@@ -6,7 +8,10 @@ store_list = []
 count = 0
 for line in fh:
     line_r = line.rstrip()
-    print("   ", line_r)
+    if not line_r.isspace():
+        print("   ", line_r)
+        if count == 0:
+            print("-"*35)
 
 
     if "ᵒ" in line_r:
@@ -23,7 +28,17 @@ for line in fh:
         d1 = float(angler) + (float(minute) * 0.0166666667) + (float(sec) * 0.000277778)
         store_list.append(d1)
         count += 1
-    if count == 2:
+    if "-" in line_r:
+        # float from GD
+        num = float(line_r.replace('-', '.'))
+        angler_from_GD = num * 6
+
+
+        d1 = angler_from_GD
+        store_list.append(d1)
+        count += 1
+
+    if count == 2 and "ᵒ" in line_r:
         sum_ang = store_list[0]+store_list[1]
         store_list = []
         count = 0
@@ -44,7 +59,62 @@ for line in fh:
             sec -= 60
         if sum_ang >= 360:
             sum_ang -= 360
-            print("-->", "{0}ᵒ{1}'{2}\"".format(int(sum_ang), int(min), round(sec)))
-        else:
-            # sum
-            print("-->", "{0}ᵒ{1}'{2}\"".format(int(sum_ang), int(min), round(sec)))
+
+        # radians
+        radians = (float(sum_ang) * math.pi) / 180
+
+        # БДУ
+        GD = float(sum_ang) / 6
+
+        print("-" * 35, sep='\n')
+        print("sum", "{0}ᵒ{1}'{2}\"".format(int(sum_ang), int(min), round(sec)))
+        print("-" * 35, sep='\n')
+        print("            degree -->", '%.5f' % sum_ang)
+        print("   mask 000ᵒ00'00\" -->", "{0}ᵒ{1}'{2}\"".format(int(sum_ang), int(min), int(float(sec))))
+        print("   mask 000ᵒ00.00' -->", "{0}ᵒ{1}'".format(int(sum_ang), round(min, 2)))
+        print("           radians -->", '%.2f' % radians)
+        print("                GD -->", (str(round(GD, 2)).replace(".", '-')))
+        print("-" * 35, sep='\n')
+        print("\n")
+    if count == 2 and "-" in line_r:
+        sum_ang = store_list[0]+store_list[1]
+        if sum_ang >= 360:
+            sum_ang -= 360
+        sum_ang = sum_ang / 6
+        store_list = []
+        count = 0
+        # float минуты
+        min_f_from_GD = sum_ang - int(sum_ang)
+
+        min_from_GD = min_f_from_GD * 60
+        # целые минуты
+        int_min_GD = int(min_from_GD)
+        # доли минут для перевода в секунды
+        min_GD_float = min_from_GD - int_min_GD
+        # перевод долей градуса в секунды
+        sec_GD = min_GD_float * 60
+
+        if math.ceil(sec_GD * 100) / 100 >= 60:
+            int_min_GD += 1
+            sec_GD = 0
+        # минуты с долями
+        min_sec = int_min_GD + sec_GD / 60
+        # print(angler_from_GD)
+
+        # radians
+        radians_GD = (float(sum_ang) * math.pi) / 180
+
+
+        print("-" * 35, sep='\n')
+        # print("original str:", line_r)
+        print("sum", (str(round(sum_ang, 2)).replace(".", '-')))
+        print("-" * 35)
+        print("            degree -->", '%.5f' % sum_ang)
+        print("   mask 000ᵒ00'00\" -->", "{0}ᵒ{1}'{2}\"".format(int(sum_ang), int_min_GD, int(float(sec_GD))))
+        print("   mask 000ᵒ00.00' -->", "{0}ᵒ{1}'".format(int(sum_ang), round(min_sec, 2)))
+        print("           radians -->", '%.2f' % radians_GD)
+        print("                GD -->", (str(round(sum_ang, 2)).replace(".", '-')))
+        print("-" * 35, sep='\n')
+        print("\n")
+
+
